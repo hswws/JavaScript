143332a6e3f01b3816170f5fb2e247fffb154a5d
hswws <62366697+hswws@users.noreply.github.com>
2020-08-04 15:43:16 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: Zite
+ * @Date: 2020-08-04 13:20:43
+ * @LastEditTime: 2020-08-04 13:22:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \TG\test.js
+ */
+alert("外部JS引用")
